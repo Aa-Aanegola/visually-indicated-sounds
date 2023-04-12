2329f4f6a642f3eb64686be906c80e1be1936f5b
Dhruv Kapur <dhruvkapur4@gmail.com>
2023-04-12 15:36:59 +0530

    Fixed bug in fetching scripts

--- fetch_test.sh
+++ fetch_test.sh
@@ -1,7 +1,7 @@
 if [ ! -d "/scratch/vis_data/test" ]; then
 	mkdir -p /scratch/vis_data
 	rsync -P ada:/share1/$USER/vis_data/test.zip /scratch/vis_data/
-	unzip /scratch/vis_data/test.zip -d /scratch/vis_data/test
+	unzip /scratch/vis_data/test.zip -d /scratch/vis_data
 	rm /scratch/vis_data/test.zip
 else
 	echo "Test Data Already Present"
--- fetch_train.sh
+++ fetch_train.sh
@@ -1,7 +1,7 @@
 if [ ! -d "/scratch/vis_data/train" ]; then
 	mkdir -p /scratch/vis_data
 	rsync -P ada:/share1/$USER/vis_data/train.zip /scratch/vis_data/
-	unzip /scratch/vis_data/train.zip -d /scratch/vis_data/train
+	unzip /scratch/vis_data/train.zip -d /scratch/vis_data
 	rm /scratch/vis_data/train.zip
 else
 	echo "Train Data Already Present"
